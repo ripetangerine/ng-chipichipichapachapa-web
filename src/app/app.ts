2486a4ff31d7cbc50a01080541acdd5be9b79922
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-21 10:59:25 +0900

    feat: behavior mouseListener e

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorService } from './components/service/behavior.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('first-chipi-ng');
+
+  constructor(private behaviorService: BehaviorService){}
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent){
+    console.log(e.clientX, e.clientY);
+    this.behaviorService.updateMouse(e.clientX, e.clientY);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(e: MouseEvent){
+    console.log(`clicked in : ${e.clientX, e.clientY}`);
+  }
 }
--- src/app/components/service/behavior.service.ts
+++ src/app/components/service/behavior.service.ts
@@ -9,19 +9,27 @@ export class BehaviorService {
     clickCount: 0,
     message: '얌전히 있어라'
   });
+  private mousePosition = new BehaviorSubject({x: 0, y: 0});
 
   behavior$ = this.behaviorSubject.asObservable();
+  mouse$ = this.mousePosition.asObservable();
 
   increaseClick() {
     const current = this.behaviorSubject.value;
-
     const newState = {
       clickCount: current.clickCount + 1,
       message: current.clickCount > 5 
         ? '좀 그만해…' 
         : '좋다 더 눌러봐'
     };
-
     this.behaviorSubject.next(newState);
   }
+
+  updateMouse(x: number, y: number){
+    this.mousePosition.next({x, y});
+  }
+
+  // ngOnInit() {
+  
+  // }
 }
