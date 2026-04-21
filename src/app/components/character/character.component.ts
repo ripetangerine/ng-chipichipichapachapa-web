b44b25ab80c3b979106e606a7cd214b09b4b5cbe
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-21 15:08:45 +0900

    chore: update component ts before refactor

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -4,30 +4,48 @@ import { interval } from "rxjs";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { AsyncPipe, DatePipe } from "@angular/common";
 
-// 
 
 @Component({
-  imports: [AsyncPipe, DatePipe],
+  // imports: [AsyncPipe, DatePipe],
   selector: 'app-character',
   templateUrl: './character.component.html'
 })
 export class CharacterComponent {
-  stackTimer = signal(0);
-
-  chipiCatImagePath = signal("");
+  
+  chipiCatImagePathEven = 'assets/chipiCatEven.jpg';
+  chipiCatImagePathOdd = 'assets/chipiCatOdd.jpg'
+  
+  chipiCatImagePath = this.chipiCatImagePathOdd;
   dancingTime = signal(""); // 자리수 처리 0:00:00
   dancingTimeLongest = signal(""); // 움직이지 않는 시점에 긴지 확인후 넣기
   msMoveCount = signal(0);
   isMoving = signal(false);
-  
+  stackTimer = signal(0);
+
+  // private behaviorService = inject(BehaviorService);
   constructor(private behaviorService: BehaviorService) { // 썌얘
     interval(1000)
       .pipe(takeUntilDestroyed())
       .subscribe(()=>{
-        this.stackTimer.update(v=>v+1);
+        this.stackTimer.update(v=>{
+          return v+1;
+        });
       });
   }
 
+  //handler
+  /**
+   * stackTimer에 쌓일때
+   * - 마우스 움직임 확인 (움직인다면 속도에 비례한 카운트 증가, 움직임 변수 활성화)
+   * - 1초에 한번 치피캣 움직임 (사용자 빠르게 움직이면 얘도 0.5초에 한 번 움직임)
+   * - dancingTime 증가
+   * 
+   * 움직임이 멈춘다면
+   * - isMoving -> false
+   * - dancingTime -> 0
+   * - dancingTimeLongest 등록
+   * - 치피캣 멈춤
+   */
 
 
 
@@ -37,8 +55,5 @@ export class CharacterComponent {
     this.behaviorService.status$.subscribe(data => {
       this.dancingTimeLongest = data.dacingTimeLongest as unknown as WritableSignal<string>;
     });
-
-    
-
   }
 }
